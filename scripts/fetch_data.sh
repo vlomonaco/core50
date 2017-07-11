f76a52418c09d055317de89ab19378b497194569
vlomonaco <vincenzo.lomonaco@outlook.com>
2017-07-11 11:24:03 +0200

    extras added

---
+++ scripts/fetch_data.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Downloading caffenet pre-trained model..."
+wget --directory-prefix='../data/' http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
+
+echo "Downloading VGG_CNN_M pre-trained model..."
+wget --directory-prefix='../data/' http://www.robots.ox.ac.uk/%7Evgg/software/deep_eval/releases/bvlc/VGG_CNN_M.caffemodel
+
+echo "Downloading Core50 (128x128 version)..."
+wget --directory-prefix='../data/' http://bias.csr.unibo.it/maltoni/download/core50/core50_128x128.zip
+
+echo "Unzipping Core50..."
+unzip ../data/core50_128x128.zipfile.zip -d ../data/
